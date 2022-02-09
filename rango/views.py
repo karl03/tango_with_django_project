05f9cf934481d33716d004a42f97a524fdafc67d
Karl Hartmann <2566374h@student.gla.ac.uk>
2022-02-09 16:11:36 +0000

    Chapter 4 complete

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Rango says hey there partner! <a href="/rango/about/">About</a>')
+    # Dictionary to pass to template, key is is the name of the modifiable var
+    # in the template, value is what is passed into this var
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # Renders the template with the message I have passed in
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse('Rango says here is the about page. <a href="/rango/">Index</a>')
+    context_dict = {'boldmessage' : 'This tutorial has been put together by Karl'}
+    return render(request, 'rango/about.html', context=context_dict)
