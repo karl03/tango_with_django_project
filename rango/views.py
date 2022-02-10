5e6304669a49c97bb7edcdbcc76d6681a5501954
Karl Hartmann <2566374h@student.gla.ac.uk>
2022-02-10 01:28:32 +0000

    Chapter 6 complete

--- rango/views.py
+++ rango/views.py
@@ -1,12 +1,41 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from rango.models import Category
+from rango.models import Page
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+
+        context_dict['category'] = category
+        
+    except Category.DoesNotExist:
+        
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context=context_dict)
+
+        
 
 def index(request):
-    # Dictionary to pass to template, key is is the name of the modifiable var
-    # in the template, value is what is passed into this var
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-    # Renders the template with the message I have passed in
+    #get top 5 most liked categories
+    category_list = Category.objects.order_by('-likes')[:5]
+    #get top 5 most viewed pages
+    page_list = Page.objects.order_by('-views')[:5]
+
+    #create dictionary to pass into the template
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
+    # Renders the template with the information I have passed in
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
